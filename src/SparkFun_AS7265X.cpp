4eb38e72dd01ac943a71b0a6f8775b14a0fffacc
Nathan Seidle <nathan@sparkfun.com>
2021-08-27 10:44:44 -0600

    Add polling delay to isConnected.
    AS7265x takes ~660ms to respond from POR.

--- src/SparkFun_AS7265X.cpp
+++ src/SparkFun_AS7265X.cpp
@@ -28,7 +28,6 @@
 //Constructor
 AS7265X::AS7265X()
 {
-
 }
 
 //Initializes the sensor with basic settings
@@ -38,11 +37,13 @@ boolean AS7265X::begin(TwoWire &wirePort)
   _i2cPort = &wirePort;
   _i2cPort->begin(); //This resets any setClock() the user may have done
 
-  if (isConnected() == false) return (false); //Check for sensor presence
+  if (isConnected() == false)
+    return (false); //Check for sensor presence
 
   //Check to see if both slaves are detected
   uint8_t value = virtualReadRegister(AS7265X_DEV_SELECT_CONTROL);
-  if ( (value & 0b00110000) == 0) return (false); //Test if Slave1 and 2 are detected. If not, bail.
+  if ((value & 0b00110000) == 0)
+    return (false); //Test if Slave1 and 2 are detected. If not, bail.
 
   setBulbCurrent(AS7265X_LED_CURRENT_LIMIT_12_5MA, AS7265x_LED_WHITE);
   setBulbCurrent(AS7265X_LED_CURRENT_LIMIT_12_5MA, AS7265x_LED_IR);
@@ -61,7 +62,7 @@ boolean AS7265X::begin(TwoWire &wirePort)
   setGain(AS7265X_GAIN_64X); //Set gain to 64x
 
   setMeasurementMode(AS7265X_MEASUREMENT_MODE_6CHAN_ONE_SHOT); //One-shot reading of VBGYOR
-  
+
   enableInterrupt();
 
   return (true); //We're all setup!
@@ -69,44 +70,49 @@ boolean AS7265X::begin(TwoWire &wirePort)
 
 uint8_t AS7265X::getDeviceType()
 {
-  return(virtualReadRegister(AS7265X_HW_VERSION_HIGH));
+  return (virtualReadRegister(AS7265X_HW_VERSION_HIGH));
 }
 uint8_t AS7265X::getHardwareVersion()
 {
-  return(virtualReadRegister(AS7265X_HW_VERSION_LOW));
+  return (virtualReadRegister(AS7265X_HW_VERSION_LOW));
 }
 
 uint8_t AS7265X::getMajorFirmwareVersion()
 {
   virtualWriteRegister(AS7265X_FW_VERSION_HIGH, 0x01); //Set to 0x01 for Major
-  virtualWriteRegister(AS7265X_FW_VERSION_LOW, 0x01); //Set to 0x01 for Major
-  
-  return(virtualReadRegister(AS7265X_FW_VERSION_LOW));
+  virtualWriteRegister(AS7265X_FW_VERSION_LOW, 0x01);  //Set to 0x01 for Major
+
+  return (virtualReadRegister(AS7265X_FW_VERSION_LOW));
 }
 
 uint8_t AS7265X::getPatchFirmwareVersion()
 {
   virtualWriteRegister(AS7265X_FW_VERSION_HIGH, 0x02); //Set to 0x02 for Patch
-  virtualWriteRegister(AS7265X_FW_VERSION_LOW, 0x02); //Set to 0x02 for Patch
-  
-  return(virtualReadRegister(AS7265X_FW_VERSION_LOW));
+  virtualWriteRegister(AS7265X_FW_VERSION_LOW, 0x02);  //Set to 0x02 for Patch
+
+  return (virtualReadRegister(AS7265X_FW_VERSION_LOW));
 }
 
 uint8_t AS7265X::getBuildFirmwareVersion()
 {
   virtualWriteRegister(AS7265X_FW_VERSION_HIGH, 0x03); //Set to 0x03 for Build
-  virtualWriteRegister(AS7265X_FW_VERSION_LOW, 0x03); //Set to 0x03 for Build
-  
-  return(virtualReadRegister(AS7265X_FW_VERSION_LOW));
+  virtualWriteRegister(AS7265X_FW_VERSION_LOW, 0x03);  //Set to 0x03 for Build
+
+  return (virtualReadRegister(AS7265X_FW_VERSION_LOW));
 }
 
 //Returns true if I2C device ack's
 boolean AS7265X::isConnected()
 {
-  _i2cPort->beginTransmission((uint8_t)AS7265X_ADDR);
-  if (_i2cPort->endTransmission() != 0)
-    return (false); //Sensor did not ACK
-  return (true);
+  //Give IC 660ms for startup - max 1000ms
+  for (uint8_t x = 0; x < 100; x++)
+  {
+    _i2cPort->beginTransmission((uint8_t)AS7265X_ADDR);
+    if (_i2cPort->endTransmission() == 0)
+      return (true); //Sensor ACK'd
+    delay(10);
+  }
+  return (false); //Sensor did not ACK
 }
 
 //Tells IC to take all channel measurements and polls for data ready flag
@@ -115,7 +121,8 @@ void AS7265X::takeMeasurements()
   setMeasurementMode(AS7265X_MEASUREMENT_MODE_6CHAN_ONE_SHOT); //Set mode to all 6-channels, one-shot
 
   //Wait for data to be ready
-  while (dataAvailable() == false) delay(AS7265X_POLLING_DELAY);
+  while (dataAvailable() == false)
+    delay(AS7265X_POLLING_DELAY);
 
   //Readings can now be accessed via getCalibratedA(), getJ(), etc
 }
@@ -135,62 +142,80 @@ void AS7265X::takeMeasurementsWithBulb()
 }
 
 //Get the various color readings
-uint16_t AS7265X::getG() {
+uint16_t AS7265X::getG()
+{
   return (getChannel(AS7265X_R_G_A, AS72652_VISIBLE));
 }
-uint16_t AS7265X::getH() {
+uint16_t AS7265X::getH()
+{
   return (getChannel(AS7265X_S_H_B, AS72652_VISIBLE));
 }
-uint16_t AS7265X::getI() {
+uint16_t AS7265X::getI()
+{
   return (getChannel(AS7265X_T_I_C, AS72652_VISIBLE));
 }
-uint16_t AS7265X::getJ() {
+uint16_t AS7265X::getJ()
+{
   return (getChannel(AS7265X_U_J_D, AS72652_VISIBLE));
 }
-uint16_t AS7265X::getK() {
+uint16_t AS7265X::getK()
+{
   return (getChannel(AS7265X_V_K_E, AS72652_VISIBLE));
 }
-uint16_t AS7265X::getL() {
+uint16_t AS7265X::getL()
+{
   return (getChannel(AS7265X_W_L_F, AS72652_VISIBLE));
 }
 
 //Get the various NIR readings
-uint16_t AS7265X::getR() {
+uint16_t AS7265X::getR()
+{
   return (getChannel(AS7265X_R_G_A, AS72651_NIR));
 }
-uint16_t AS7265X::getS() {
+uint16_t AS7265X::getS()
+{
   return (getChannel(AS7265X_S_H_B, AS72651_NIR));
 }
-uint16_t AS7265X::getT() {
+uint16_t AS7265X::getT()
+{
   return (getChannel(AS7265X_T_I_C, AS72651_NIR));
 }
-uint16_t AS7265X::getU() {
+uint16_t AS7265X::getU()
+{
   return (getChannel(AS7265X_U_J_D, AS72651_NIR));
 }
-uint16_t AS7265X::getV() {
+uint16_t AS7265X::getV()
+{
   return (getChannel(AS7265X_V_K_E, AS72651_NIR));
 }
-uint16_t AS7265X::getW() {
+uint16_t AS7265X::getW()
+{
   return (getChannel(AS7265X_W_L_F, AS72651_NIR));
 }
 
 //Get the various UV readings
-uint16_t AS7265X::getA() {
+uint16_t AS7265X::getA()
+{
   return (getChannel(AS7265X_R_G_A, AS72653_UV));
 }
-uint16_t AS7265X::getB() {
+uint16_t AS7265X::getB()
+{
   return (getChannel(AS7265X_S_H_B, AS72653_UV));
 }
-uint16_t AS7265X::getC() {
+uint16_t AS7265X::getC()
+{
   return (getChannel(AS7265X_T_I_C, AS72653_UV));
 }
-uint16_t AS7265X::getD() {
+uint16_t AS7265X::getD()
+{
   return (getChannel(AS7265X_U_J_D, AS72653_UV));
 }
-uint16_t AS7265X::getE() {
+uint16_t AS7265X::getE()
+{
   return (getChannel(AS7265X_V_K_E, AS72653_UV));
 }
-uint16_t AS7265X::getF() {
+uint16_t AS7265X::getF()
+{
   return (getChannel(AS7265X_W_L_F, AS72653_UV));
 }
 
@@ -199,67 +224,84 @@ uint16_t AS7265X::getChannel(uint8_t channelRegister, uint8_t device)
 {
   selectDevice(device);
   uint16_t colorData = virtualReadRegister(channelRegister) << 8; //High uint8_t
-  colorData |= virtualReadRegister(channelRegister + 1); //Low uint8_t
+  colorData |= virtualReadRegister(channelRegister + 1);          //Low uint8_t
   return (colorData);
 }
 
 //Returns the various calibration data
-float AS7265X::getCalibratedA() {
+float AS7265X::getCalibratedA()
+{
   return (getCalibratedValue(AS7265X_R_G_A_CAL, AS72653_UV));
 }
-float AS7265X::getCalibratedB() {
+float AS7265X::getCalibratedB()
+{
   return (getCalibratedValue(AS7265X_S_H_B_CAL, AS72653_UV));
 }
-float AS7265X::getCalibratedC() {
+float AS7265X::getCalibratedC()
+{
   return (getCalibratedValue(AS7265X_T_I_C_CAL, AS72653_UV));
 }
-float AS7265X::getCalibratedD() {
+float AS7265X::getCalibratedD()
+{
   return (getCalibratedValue(AS7265X_U_J_D_CAL, AS72653_UV));
 }
-float AS7265X::getCalibratedE() {
+float AS7265X::getCalibratedE()
+{
   return (getCalibratedValue(AS7265X_V_K_E_CAL, AS72653_UV));
 }
-float AS7265X::getCalibratedF() {
+float AS7265X::getCalibratedF()
+{
   return (getCalibratedValue(AS7265X_W_L_F_CAL, AS72653_UV));
 }
 
-
 //Returns the various calibration data
-float AS7265X::getCalibratedG() {
+float AS7265X::getCalibratedG()
+{
   return (getCalibratedValue(AS7265X_R_G_A_CAL, AS72652_VISIBLE));
 }
-float AS7265X::getCalibratedH() {
+float AS7265X::getCalibratedH()
+{
   return (getCalibratedValue(AS7265X_S_H_B_CAL, AS72652_VISIBLE));
 }
-float AS7265X::getCalibratedI() {
+float AS7265X::getCalibratedI()
+{
   return (getCalibratedValue(AS7265X_T_I_C_CAL, AS72652_VISIBLE));
 }
-float AS7265X::getCalibratedJ() {
+float AS7265X::getCalibratedJ()
+{
   return (getCalibratedValue(AS7265X_U_J_D_CAL, AS72652_VISIBLE));
 }
-float AS7265X::getCalibratedK() {
+float AS7265X::getCalibratedK()
+{
   return (getCalibratedValue(AS7265X_V_K_E_CAL, AS72652_VISIBLE));
 }
-float AS7265X::getCalibratedL() {
+float AS7265X::getCalibratedL()
+{
   return (getCalibratedValue(AS7265X_W_L_F_CAL, AS72652_VISIBLE));
 }
 
-float AS7265X::getCalibratedR() {
+float AS7265X::getCalibratedR()
+{
   return (getCalibratedValue(AS7265X_R_G_A_CAL, AS72651_NIR));
 }
-float AS7265X::getCalibratedS() {
+float AS7265X::getCalibratedS()
+{
   return (getCalibratedValue(AS7265X_S_H_B_CAL, AS72651_NIR));
 }
-float AS7265X::getCalibratedT() {
+float AS7265X::getCalibratedT()
+{
   return (getCalibratedValue(AS7265X_T_I_C_CAL, AS72651_NIR));
 }
-float AS7265X::getCalibratedU() {
+float AS7265X::getCalibratedU()
+{
   return (getCalibratedValue(AS7265X_U_J_D_CAL, AS72651_NIR));
 }
-float AS7265X::getCalibratedV() {
+float AS7265X::getCalibratedV()
+{
   return (getCalibratedValue(AS7265X_V_K_E_CAL, AS72651_NIR));
 }
-float AS7265X::getCalibratedW() {
+float AS7265X::getCalibratedW()
+{
   return (getCalibratedValue(AS7265X_W_L_F_CAL, AS72651_NIR));
 }
 
@@ -298,13 +340,14 @@ float AS7265X::convertBytesToFloat(uint32_t myLong)
 //Mode 3: One-shot reading of all channels
 void AS7265X::setMeasurementMode(uint8_t mode)
 {
-  if (mode > 0b11) mode = 0b11; //Error check
+  if (mode > 0b11)
+    mode = 0b11; //Error check
 
   //Read, mask/set, write
   uint8_t value = virtualReadRegister(AS7265X_CONFIG); //Read
-  value &= 0b11110011; //Clear BANK bits
-  value |= (mode << 2); //Set BANK bits with user's choice
-  virtualWriteRegister(AS7265X_CONFIG, value); //Write
+  value &= 0b11110011;                                 //Clear BANK bits
+  value |= (mode << 2);                                //Set BANK bits with user's choice
+  virtualWriteRegister(AS7265X_CONFIG, value);         //Write
 }
 
 //Sets the gain value
@@ -314,13 +357,14 @@ void AS7265X::setMeasurementMode(uint8_t mode)
 //Gain 3: 64x
 void AS7265X::setGain(uint8_t gain)
 {
-  if (gain > 0b11) gain = 0b11;
+  if (gain > 0b11)
+    gain = 0b11;
 
   //Read, mask/set, write
   uint8_t value = virtualReadRegister(AS7265X_CONFIG); //Read
-  value &= 0b11001111; //Clear GAIN bits
-  value |= (gain << 4); //Set GAIN bits with user's choice
-  virtualWriteRegister(AS7265X_CONFIG, value); //Write
+  value &= 0b11001111;                                 //Clear GAIN bits
+  value |= (gain << 4);                                //Set GAIN bits with user's choice
+  virtualWriteRegister(AS7265X_CONFIG, value);         //Write
 }
 
 //Sets the integration cycle amount
@@ -335,8 +379,8 @@ void AS7265X::enableInterrupt()
 {
   //Read, mask/set, write
   uint8_t value = virtualReadRegister(AS7265X_CONFIG); //Read
-  value |= (1 << 6); //Set INT bit
-  virtualWriteRegister(AS7265X_CONFIG, value); //Write
+  value |= (1 << 6);                                   //Set INT bit
+  virtualWriteRegister(AS7265X_CONFIG, value);         //Write
 }
 
 //Disables the interrupt pin
@@ -344,8 +388,8 @@ void AS7265X::disableInterrupt()
 {
   //Read, mask/set, write
   uint8_t value = virtualReadRegister(AS7265X_CONFIG); //Read
-  value &= ~(1 << 6); //Clear INT bit
-  virtualWriteRegister(AS7265X_CONFIG, value); //Write
+  value &= ~(1 << 6);                                  //Clear INT bit
+  virtualWriteRegister(AS7265X_CONFIG, value);         //Write
 }
 
 //Checks to see if DRDY flag is set in the control setup register
@@ -387,15 +431,17 @@ void AS7265X::setBulbCurrent(uint8_t current, uint8_t device)
   selectDevice(device);
 
   // set the current
-  if (current > 0b11) current = 0b11; //Limit to two bits
+  if (current > 0b11)
+    current = 0b11;                                        //Limit to two bits
   uint8_t value = virtualReadRegister(AS7265X_LED_CONFIG); //Read
-  value &= 0b11001111; //Clear ICL_DRV bits
-  value |= (current << 4); //Set ICL_DRV bits with user's choice
-  virtualWriteRegister(AS7265X_LED_CONFIG, value); //Write
+  value &= 0b11001111;                                     //Clear ICL_DRV bits
+  value |= (current << 4);                                 //Set ICL_DRV bits with user's choice
+  virtualWriteRegister(AS7265X_LED_CONFIG, value);         //Write
 }
 
 //As we read various registers we have to point at the master or first/second slave
-void AS7265X::selectDevice(uint8_t device) {
+void AS7265X::selectDevice(uint8_t device)
+{
   //Set the bits 0:1. Just overwrite whatever is there because masking in the correct value doesn't work.
   virtualWriteRegister(AS7265X_DEV_SELECT_CONTROL, device);
 
@@ -431,16 +477,16 @@ void AS7265X::disableIndicator()
 //Set the current limit of onboard LED. Default is max 8mA = 0b11.
 void AS7265X::setIndicatorCurrent(uint8_t current)
 {
-  if (current > 0b11) current = 0b11;
+  if (current > 0b11)
+    current = 0b11;
   //Read, mask/set, write
   uint8_t value = virtualReadRegister(AS7265X_LED_CONFIG); //Read
-  value &= 0b11111001; //Clear ICL_IND bits
-  value |= (current << 1); //Set ICL_IND bits with user's choice
+  value &= 0b11111001;                                     //Clear ICL_IND bits
+  value |= (current << 1);                                 //Set ICL_IND bits with user's choice
 
   selectDevice(AS72651_NIR);
   virtualWriteRegister(AS7265X_LED_CONFIG, value); //Write
 }
-
 
 //Returns the temperature of a given device in C
 uint8_t AS7265X::getTemperature(uint8_t deviceNumber)
@@ -454,10 +500,10 @@ float AS7265X::getTemperatureAverage()
 {
   float average = 0;
 
-  for(uint8_t x = 0 ; x < 3 ; x++)
+  for (uint8_t x = 0; x < 3; x++)
     average += getTemperature(x);
-  
-  return (average/3);
+
+  return (average / 3);
 }
 
 //Does a soft reset
@@ -466,8 +512,8 @@ void AS7265X::softReset()
 {
   //Read, mask/set, write
   uint8_t value = virtualReadRegister(AS7265X_CONFIG); //Read
-  value |= (1 << 7); //Set RST bit, automatically cleared after reset
-  virtualWriteRegister(AS7265X_CONFIG, value); //Write
+  value |= (1 << 7);                                   //Set RST bit, automatically cleared after reset
+  virtualWriteRegister(AS7265X_CONFIG, value);         //Write
 }
 
 //Read a virtual register from the AS7265x
@@ -479,14 +525,15 @@ uint8_t AS7265X::virtualReadRegister(uint8_t virtualAddr)
   status = readRegister(AS7265X_STATUS_REG);
   if ((status & AS7265X_RX_VALID) != 0) //There is data to be read
   {
-    uint8_t incoming = readRegister(AS7265X_READ_REG); //Read the byte but do nothing with it
+    readRegister(AS7265X_READ_REG); //Read the byte but do nothing with it
   }
 
   //Wait for WRITE flag to clear
   while (1)
   {
     status = readRegister(AS7265X_STATUS_REG);
-    if ((status & AS7265X_TX_VALID) == 0) break; // If TX bit is clear, it is ok to write
+    if ((status & AS7265X_TX_VALID) == 0)
+      break; // If TX bit is clear, it is ok to write
     delay(AS7265X_POLLING_DELAY);
   }
 
@@ -497,7 +544,8 @@ uint8_t AS7265X::virtualReadRegister(uint8_t virtualAddr)
   while (1)
   {
     status = readRegister(AS7265X_STATUS_REG);
-    if ((status & AS7265X_RX_VALID) != 0) break; // Read data is ready.
+    if ((status & AS7265X_RX_VALID) != 0)
+      break; // Read data is ready.
     delay(AS7265X_POLLING_DELAY);
   }
 
@@ -514,18 +562,20 @@ void AS7265X::virtualWriteRegister(uint8_t virtualAddr, uint8_t dataToWrite)
   while (1)
   {
     status = readRegister(AS7265X_STATUS_REG);
-    if ((status & AS7265X_TX_VALID) == 0) break; // No inbound TX pending at slave. Okay to write now.
+    if ((status & AS7265X_TX_VALID) == 0)
+      break; // No inbound TX pending at slave. Okay to write now.
     delay(AS7265X_POLLING_DELAY);
   }
 
   // Send the virtual register address (setting bit 7 to indicate we are writing to a register).
-  writeRegister(AS7265X_WRITE_REG, (virtualAddr | 1<<7));
+  writeRegister(AS7265X_WRITE_REG, (virtualAddr | 1 << 7));
 
   //Wait for WRITE register to be empty
   while (1)
   {
     status = readRegister(AS7265X_STATUS_REG);
-    if ((status & AS7265X_TX_VALID) == 0) break; // No inbound TX pending at slave. Okay to write now.
+    if ((status & AS7265X_TX_VALID) == 0)
+      break; // No inbound TX pending at slave. Okay to write now.
     delay(AS7265X_POLLING_DELAY);
   }
 
@@ -545,7 +595,8 @@ uint8_t AS7265X::readRegister(uint8_t addr)
   }
 
   _i2cPort->requestFrom((uint8_t)AS7265X_ADDR, (uint8_t)1);
-  if (_i2cPort->available()) {
+  if (_i2cPort->available())
+  {
     return (_i2cPort->read());
   }
 
